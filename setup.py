54ee1e521602686903b98dc95cdca26b09a45ef2
Jon Dufresne <jon.dufresne@gmail.com>
2017-10-22 11:50:03 -0700

    Add additional trove classifiers for support Python version

--- setup.py
+++ setup.py
@@ -24,10 +24,12 @@ setup(
 		'Intended Audience :: Developers',
 		'Topic :: Software Development :: Libraries :: Python Modules',
 
+		'Programming Language :: Python :: 2',
 		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3',
 		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
 	],
 
 	keywords="email address validator",
