f49296323b99b7e7239a0b8362c9951e3b2394e8
Osvaldo Santana Neto <osantana@osantana.me>
2018-11-05 17:12:30 -0200

    Add support for timeout specification

--- email_validator/__init__.py
+++ email_validator/__init__.py
@@ -55,7 +55,8 @@ def validate_email(
 	email,
 	allow_smtputf8=True,
 	allow_empty_local=False,
-	check_deliverability=True):
+	check_deliverability=True,
+	timeout=0):
 
 	"""Validates an email address, raising an EmailNotValidError if the address is not valid or returning a dict of information
 	when the address is valid. The email argument can be a str or a bytes instance, but if bytes it must be ASCII-only."""
@@ -88,7 +89,7 @@ def validate_email(
 	if check_deliverability:
 		# Validate the email address's deliverability and update the
 		# return dict with metadata.
-		ret.update(validate_email_deliverability(ret["domain"], ret["domain_i18n"]))
+		ret.update(validate_email_deliverability(ret["domain"], ret["domain_i18n"], timeout))
 
 	# If the email address has an ASCII form, add it.
 	ret["email"] = ret["local"] + "@" + ret["domain_i18n"]
@@ -233,12 +234,15 @@ def validate_email_domain_part(domain):
 		"domain_i18n": domain_i18n,
 	}
 
-def validate_email_deliverability(domain, domain_i18n):
+def validate_email_deliverability(domain, domain_i18n, timeout=0):
 	# Check that the domain resolves to an MX record. If there is no MX record,
 	# try an A or AAAA record which is a deprecated fallback for deliverability.
 
 	try:
 		resolver = dns.resolver.get_default_resolver()
+
+		if timeout:
+			resolver.lifetime = timeout
 
 		try:
 			# Try resolving for MX records and get them in sorted priority order.
@@ -260,7 +264,7 @@ def validate_email_deliverability(domain, domain_i18n):
 					mtas = [(0, str(r)) for r in response]
 					mx_fallback = "AAAA"
 				except (dns.resolver.NoNameservers, dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
-					
+
 					# If there was no MX, A, or AAAA record, then mail to
 					# this domain is not deliverable.
 					raise EmailUndeliverableError("The domain name %s does not exist." % domain_i18n)
