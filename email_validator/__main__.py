18106ca560a8acbfa89a1e8d1330ca3a07108c2d
Joshua Tauberer <jt@occams.info>
2023-04-09 10:54:47 -0400

    In the __main__ tool read options to validate_email from environment variables

--- email_validator/__main__.py
+++ email_validator/__main__.py
@@ -10,8 +10,12 @@
 # invalid email addresses. When passing an email address on the command
 # line, if the email address is valid, information about it will be printed.
 # When using STDIN, no output will be given for valid email addresses.
+#
+# Keyword arguments to validate_email can be set in environment variables
+# of the same name but upprcase (see below).
 
 import json
+import os
 import sys
 
 from .validate_email import validate_email
@@ -22,20 +26,30 @@ from .exceptions_types import EmailNotValidError
 def main(dns_resolver=None):
     # The dns_resolver argument is for tests.
 
+    # Set options from environment variables.
+    options = {}
+    for varname in ('ALLOW_SMTPUTF8', 'ALLOW_QUOTED_LOCAL', 'GLOBALLY_DELIVERABLE',
+                    'CHECK_DELIVERABILITY', 'TEST_ENVIRONMENT'):
+        if varname in os.environ:
+            options[varname.lower()] = bool(os.environ[varname])
+    for varname in ('DEFAULT_TIMEOUT',):
+        if varname in os.environ:
+            options[varname.lower()] = float(os.environ[varname])
+
     if len(sys.argv) == 1:
         # Validate the email addresses pased line-by-line on STDIN.
         dns_resolver = dns_resolver or caching_resolver()
         for line in sys.stdin:
             email = line.strip()
             try:
-                validate_email(email, dns_resolver=dns_resolver)
+                validate_email(email, dns_resolver=dns_resolver, **options)
             except EmailNotValidError as e:
                 print(f"{email} {e}")
     else:
         # Validate the email address passed on the command line.
         email = sys.argv[1]
         try:
-            result = validate_email(email, dns_resolver=dns_resolver)
+            result = validate_email(email, dns_resolver=dns_resolver, **options)
             print(json.dumps(result.as_dict(), indent=2, sort_keys=True, ensure_ascii=False))
         except EmailNotValidError as e:
             print(e)
