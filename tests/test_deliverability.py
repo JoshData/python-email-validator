8aa9223bdc78688c8e08d332fd0baad38c5e6d68
Joshua Tauberer <jt@occams.info>
2023-11-22 16:35:00 -0500

    Improve code coverage with a domain that returns NOANSWER instead of NXDOMAIN

--- tests/test_deliverability.py
+++ tests/test_deliverability.py
@@ -21,7 +21,7 @@ def test_deliverability_found():
 
 
 def test_deliverability_fails():
-    # No MX record.
+    # Domain does not exist.
     domain = 'xkxufoekjvjfjeodlfmdfjcu.com'
     with pytest.raises(EmailUndeliverableError, match=f'The domain name {domain} does not exist'):
         validate_email_deliverability(domain, domain, dns_resolver=RESOLVER)
@@ -34,6 +34,12 @@ def test_deliverability_fails():
     # No MX record, A record fallback, reject-all SPF record.
     domain = 'nellis.af.mil'
     with pytest.raises(EmailUndeliverableError, match=f'The domain name {domain} does not send email'):
+        validate_email_deliverability(domain, domain, dns_resolver=RESOLVER)
+
+    # No MX or A/AAAA records, but some other DNS records must
+    # exist such that the response is NOANSWER instead of NXDOMAIN.
+    domain = 'justtxt.joshdata.me'
+    with pytest.raises(EmailUndeliverableError, match=f'The domain name {domain} does not accept email'):
         validate_email_deliverability(domain, domain, dns_resolver=RESOLVER)
 
 
